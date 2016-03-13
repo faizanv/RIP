dee1991c37715d1481ca385c07d31ffc5ab6ebbe
Vishakh Kumar <vkumar@gatech.com>
2016-03-12 20:46:45 -0500

    Bug 02: Fixed override problems

--- rip.py
+++ rip.py
@@ -8,7 +8,7 @@ import re
 
 defaultVal = [
 ['retries',"--retries", "10"],
-['audioFormat','--audio-format',"mp3"],
+['audioFormat','--audio-format',"wav"],
 ['audioQuality','--audio-quality','192k'],
 ]
 
@@ -32,13 +32,28 @@ def rip(d):
     # trimming the file
     subprocess.call("sudo ffmpeg -ss "+d["startTime"]+ " -t "+d["duration"]+" -i temp.wav "+d["filename"]+".wav",shell=True)
 
+
+    # tiny bit of testing
+
+
+    #subprocess.call("if [ -f ./temp.wav ]; then rm ./temp.wav")
+    #subprocess.call("if [ -f ./input.wav ]; then rm ./input.wav")
+    subprocess.call("sudo rm temp.wav",shell=True)
+    #subprocess.call("echo 'y \n' ")
+    subprocess.call("sudo rm input.wav",shell=True)
+    #subprocess.call("echo 'y \n' ")
+    #subprocess.call("rm ")
+
+
+
+
+
     out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
     file_out = os.path.join(out_dir, d['filename']+'.wav')
     subprocess.call(['clear'], shell=True)
 
 
     return file_out
-
 
 def optionAdd(*strings):
     s = " "
@@ -54,7 +69,7 @@ d = {
 'audioFormat':'wav',
 'startTime':'0',
 'duration':'19',
-'filename':'input'
+'filename':'inp'
 }
 
 

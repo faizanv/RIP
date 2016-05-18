51000434131a84417fda80d74f0f619833a9e0a0
faizanv <faizan.virani.44@gmail.com>
2016-05-17 17:35:16 -0700

    Working with download link

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-import rip
+from flask import Flask, render_template, request, send_from_directory, send_file
+from rip import rip, removeFile
 
 app = Flask(__name__, static_url_path='')
 
@@ -15,7 +15,24 @@ def ripit():
     param["duration"] = content.get('duration')
     param["filename"] = content.get('filename')
     param["url"] = content.get('link')
-    return rip.rip(param)
+    rip(param)
+    # retval = send_from_directory('./', param["filename"] + ".wav", mimetype="audio/wav")
+    # print "Value of return"
+    # print retval
+    # return send_file("/Users/faizanv/dev/RIP/static/logo.png", attachement_filename="logo.png")
+    # path_to_file = "./dance.wav"
+    # return send_file(
+    #      path_to_file,
+    #      mimetype="audio/wav",
+    #      as_attachment=True,
+    #      attachment_filename="dance.wav")
+    return "/download/" + param["filename"] + ".wav"
+
+@app.route("/download/<file>")
+def download(file):
+    retval = send_file("./output/" + file, attachment_filename=file, mimetype='application/octet-stream')
+    removeFile(file)
+    return retval
 
 if __name__ == "__main__":
     app.run(debug=True)
--- rip.py
+++ rip.py
@@ -9,28 +9,28 @@ import re
 defaultVal = [
 ['retries',"--retries", "10"],
 ['audioFormat','--audio-format',"wav"],
-['audioQuality','--audio-quality','192k'],
+['audioQuality','--audio-quality','0'],
 ]
 
-def rip(d):
+def rip(req):
     # d is our dictionary of required values.
     #In case the dictionary does not define something,
     #we have our own thing to work.
     # defaultVal is our defaultVal dictionary of stuff.
     options = ""
     for i in defaultVal:
-        if d.get(i[0],False)==False:
+        if req.get(i[0],False)==False:
             options = optionAdd(options,i[1],i[2])
         else:
-            options = optionAdd(options,i[1],d.get(i[0]))
+            options = optionAdd(options,i[1],req.get(i[0]))
     #defaultVal stuff to add
     options = optionAdd(options,"--ignore-errors","--extract-audio","-o 'temp.%(ext)s' ","--restrict-filenames")
-    command = optionAdd("sudo youtube-dl",options,d['url'])
+    command = optionAdd("youtube-dl",options,req['url'])
     #command = "youtube-dl -i " +d['url']+ " --extract-audio --restrict-filenames --audio-format " +"wav"+ " --audio-quality "+"192k"
     print(command)
     subprocess.call(command,shell=True)
     # trimming the file
-    subprocess.call("sudo ffmpeg -ss "+d["startTime"]+ " -t "+d["duration"]+" -i temp.wav "+d["filename"]+".wav",shell=True)
+    subprocess.call("ffmpeg -ss " + req["startTime"] + " -t " + req["duration"] + " -i temp.wav output/" + req["filename"] + ".wav",shell=True)
 
 
     # tiny bit of testing
@@ -38,9 +38,9 @@ def rip(d):
 
     #subprocess.call("if [ -f ./temp.wav ]; then rm ./temp.wav")
     #subprocess.call("if [ -f ./input.wav ]; then rm ./input.wav")
-    subprocess.call("sudo rm temp.wav",shell=True)
+    subprocess.call("rm temp.wav",shell=True)
     #subprocess.call("echo 'y \n' ")
-    subprocess.call("sudo rm input.wav",shell=True)
+    # subprocess.call("rm input.wav",shell=True)
     #subprocess.call("echo 'y \n' ")
     #subprocess.call("rm ")
 
@@ -48,12 +48,13 @@ def rip(d):
 
 
 
-    out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
-    file_out = os.path.join(out_dir, d['filename']+'.wav')
-    subprocess.call(['clear'], shell=True)
+    # out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
+    # file_out = os.path.join(out_dir, req['filename']+'.wav')
+    # subprocess.call(['clear'], shell=True)
 
 
-    return file_out
+    # return file_out
+    return
 
 def optionAdd(*strings):
     s = " "
@@ -64,10 +65,14 @@ def optionAdd(*strings):
             returnString = returnString + s + strings[i]
     return returnString
 
-d = {
-'url':'https://www.youtube.com/watch?v=cRpdIrq7Rbo',
-'audioFormat':'wav',
-'startTime':'0',
-'duration':'19',
-'filename':'inp'
-}
+def removeFile(file):
+    print "rm output/" + file
+    subprocess.call("rm output/" + file,shell=True)
+
+# d = {
+# 'url':'https://www.youtube.com/watch?v=cRpdIrq7Rbo',
+# 'audioFormat':'wav',
+# 'startTime':'0',
+# 'duration':'19',
+# 'filename':'inp'
+# }
